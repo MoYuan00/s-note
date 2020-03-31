8d61949296deab502adcd96ea2c825c524308659
geekjoker-joy <geekjoker@foxmail.com>
2020-03-31 20:01:18 +0800

    update .gitignore

--- target/classes/static/js/project/remote/index.remoteJs.js
+++
@@ -1,9 +0,0 @@
-/**
- * index页面的 远程访问js代码
- */
-let indexRemoteJs = {
-    getAllUserNotes(){
-        // axios.post(url[, data[, config]])
-        return axiosInstance.get('note/getAllUserNotes');
-    }
-};
